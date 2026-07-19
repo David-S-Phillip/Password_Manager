d7094bd9fb334c22834bcdc53cadbe799fcd53c7
David Phillip <daphiscc-g-025@student.wethinkcode.co.za>
2026-07-19 18:33:45 +0200

    added 1 test in StoreTest.java

--- src/test/java/storage/StoreTest.java
+++ src/test/java/storage/StoreTest.java
@@ -1,4 +1,25 @@
 package storage;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
 public class StoreTest {
+    Account acc;
+    Store storeHashMap;
+    @BeforeEach
+    public void setUp(){
+        this.acc = new Account("Bob","5a70fc1e224f429834df9aeec8f8ad915be96fc410ac913022d56d4a1569cb69");
+        this.storeHashMap = new Store();
+    }
+
+
+    @Test
+    public void testAddAccount(){
+        Assertions.assertEquals(0,storeHashMap.getPasswordMap().size());
+        storeHashMap.addAccount(acc.getUserName(),acc);
+        Assertions.assertEquals(1,storeHashMap.getPasswordMap().size());
+    }
 }
